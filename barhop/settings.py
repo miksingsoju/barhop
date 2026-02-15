43be05aa1f1c4e3a3cab02fe69490e94cc0cd679
Mikylla Cayago <mcacayago@gmail.com>
2026-02-15 16:33:19 +0800

    Init reviews app

--- barhop/settings.py
+++ barhop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user_management',
-    'reservations'
+    'reservations',
+    'reviews',
 ]
 
 MIDDLEWARE = [
