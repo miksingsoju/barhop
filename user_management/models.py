872ecc1984b5d156282bacbfb03b0d3e480895a7
Mikylla Cayago <mcacayago@gmail.com>
2026-02-16 14:13:54 +0800

    Created Profile model

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+
+    class UserType(models.TextChoices):
+        BAR_OWNER = "OWNER", "Bar Owner"
+        BARHOPPER = "HOPPER", "Barhopper"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    email = models.EmailField(unique=True)
+    bio = models.CharField(max_length=100, blank=True)
+
+    user_type = models.CharField(
+        max_length=10,
+        choices=UserType.choices,
+        default=UserType.BARHOPPER
+    )
+
+    def __str__(self):
+        return self.user.username
