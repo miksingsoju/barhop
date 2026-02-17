d300e0c447f0ca6cf308ac07af90063afaadf8a7
snailery <87980202+snailery@users.noreply.github.com>
2026-02-17 21:20:57 +0800

    refactor: moved all templates to templates folder

--- barhop/barhop/urls.py
+++ barhop/barhop/urls.py
@@ -16,18 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', include('user_management.urls', namespace="register")),
+    path('', include('user_management.urls', namespace="auth")),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('profile/', include('user_management.urls', namespace="user_management")),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('profile/', include('user_management.urls', namespace="user_management")),
     path('bars/', include('bars.urls', namespace="bars")),
     path('reviews/', include('reviews.urls', namespace="reviews")),
     path('reservations/', include('reservations.urls', namespace="reservations")),
--- barhop/user_management/urls.py
+++ barhop/user_management/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-    path('profile/update/', views.update_profile, name="update_profile"),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/register/', views.register, name='register'),
-    path('', views.sign_in, name='sign_in'),
-   
+    path('accounts/login', views.login, name='login'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('profile/update/', views.update_profile, name="update_profile"),
 ]
 
 app_name = "user_management"
