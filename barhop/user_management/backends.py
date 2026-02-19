d9f293a89f65cd6389a33c19d7abbfe6a5b1dc13
DavidTMN <85234939+DavidTMN@users.noreply.github.com>
2026-02-19 21:31:03 +0800

    added username login

--- barhop/user_management/backends.py
+++ barhop/user_management/backends.py
@@ -5,10 +5,13 @@ class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email=username)
+            if '@' in username:
+                user = UserModel.objects.get(email=username)
+            else:
+                user = UserModel.objects.get(username=username)
         except UserModel.DoesNotExist:
             return None
-        else:
-            if user.check_password(password):
-                return user
+        
+        if user.check_password(password):
+            return user
         return None
