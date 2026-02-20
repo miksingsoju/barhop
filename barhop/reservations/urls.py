c519bf02607afe46bc0d5c38008ecf181a7d9b5b
ar-dimatulac <adara.dimatulac@student.ateneo.edu>
2026-02-20 14:50:57 +0800

    add: initial create seating form view

--- barhop/reservations/urls.py
+++ barhop/reservations/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+    # temporary url
+    path('seating/create', views.create_seating, name='create-seating'),
 ]
 
 app_name = "reservations"
--- barhop/reservations/views.py
+++ barhop/reservations/views.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Seating, Table
 
-# Create your models here.
+
+def test():
+    print("hello")
+
+
+# @login_required
+def create_seating(request):
+    if request.method == 'POST':
+        # user_name = request.POST.get("user_name", "")
+        pass
+
+    ctx = {
+        "stepper": [
+            {
+                "num": "3",
+                "title": "Define Seating",
+                "desc": "What types of seating does your bar have?"
+            },
+            {
+                "num": "4",
+                "title": "Add Tables",
+                "desc": "Setup your bar's reservation system."
+            }
+        ],
+    }
+
+    return render(request, "reservations/create-seating.html", ctx)
