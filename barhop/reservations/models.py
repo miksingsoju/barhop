afc6a9b0d53c9be9ba3d7147e2f9ea5c078ba3fc
ar-dimatulac <adara.dimatulac@student.ateneo.edu>
2026-02-20 14:50:33 +0800

    add: created seating, table models

--- barhop/reservations/models.py
+++ barhop/reservations/models.py
@@ -1,1 +1,32 @@
 from django.db import models
+
+class Seating(models.Model):
+    """A model that represents the Seating Entity."""
+
+    name = models.CharField(max_length=255)
+    capacity = models.IntegerField(default=1)
+    desc = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Returns the name of the model."""
+        return self.name
+
+    class Meta:
+        """Metadata for the model."""
+        verbose_name = 'Seating'
+
+
+class Table(models.Model):
+    """A model that represents the Table Entity."""
+
+    seating = models.ForeignKey(Seating, on_delete=models.CASCADE)
+    qty = models.IntegerField(default=1)
+    desc = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Returns the name of the model."""
+        return self.seating
+
+    class Meta:
+        """Metadata for the model."""
+        verbose_name = 'Table'
