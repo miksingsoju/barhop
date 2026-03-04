6a3653a37d2f2bde6f4bafdcb8ad702efee96fed
ar-dimatulac <adara.dimatulac@student.ateneo.edu>
2026-03-04 23:08:04 +0800

    add: reservation model

--- barhop/reservations/models.py
+++ barhop/reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user_management.models import Profile
 from bars.models import Bar
 
 
@@ -22,3 +23,24 @@ class Table(models.Model):
     class Meta:
         """Metadata for the model."""
         verbose_name = 'Table'
+
+
+class Reservation(models.Model):
+    """A model that represents the Reservation Entity."""
+
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    hopper = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    guests = models.IntegerField(default=0)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    date = models.DateField()
+    # status = 
+
+
+    def __str__(self):
+        """Returns the name of the model."""
+        return f"{self.table_type} for {self.hopper.first_name}"
+
+    class Meta:
+        """Metadata for the model."""
+        verbose_name = 'Reservation'
