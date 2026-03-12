a597931786356bfc90cb10f991c64db30010651f
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2026-03-12 16:11:23 +0800

    add: added bar owner side reservation status logic

--- barhop/reservations/urls.py
+++ barhop/reservations/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # temporary urls
-    # path('bar/<int:bar_id>', views.bar_reservations_list, name='bar-tables'),
+    path('bar/<int:bar_id>/', views.reservations_list, name='bar-tables'),
     path('bar/<int:bar_id>/tables', views.get_avail_tables, name='get-avail-tables'),
     path('bar/<int:bar_id>/create', views.create_reservation, name='create-reservation'),
     path('', views.my_reservations, name='my-reservations'),
