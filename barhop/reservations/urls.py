bf26b1ab60c25e41f257b081f223442aa3923219
DavidTMN <85234939+DavidTMN@users.noreply.github.com>
2026-03-13 21:57:00 +0800

    added manage-tables to urls.py

--- barhop/reservations/urls.py
+++ barhop/reservations/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('bar/<int:bar_id>/tables', views.get_avail_tables, name='get-avail-tables'),
     path('bar/<int:bar_id>/create', views.create_reservation, name='create-reservation'),
     path('', views.my_reservations, name='my-reservations'),
+    path('bars/<int:bar_id>/tables/manage/', views.manage_tables, name='manage-tables'),
 ]
 
 app_name = "reservations"
