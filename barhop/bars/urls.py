ab6ca038aca381300fbc0a83c554094d2324f97a
DavidTMN <85234939+DavidTMN@users.noreply.github.com>
2026-04-10 22:35:04 +0800

    feat: added paths for edit and deletion of reviews

--- barhop/bars/urls.py
+++ barhop/bars/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('create/', views.create_bar, name='create-bar'),
     path('<int:bar_id>/', views.bar_details, name='bar-details'),
     path('<int:bar_id>/update', views.bar_update, name='bar-update'),
+    path('reviews/<int:review_id>/update/', views.update_review, name='review-update'),
+    path('reviews/<int:review_id>/delete/', views.delete_review, name='review-delete'),
 ]
 
 app_name = "bars"
