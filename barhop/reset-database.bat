c3f98172dc2aa06616b677a66b82dab550710a21
ar-dimatulac <adara.dimatulac@student.ateneo.edu>
2026-04-23 20:11:26 +0800

    fix: updated batch files to include populate db script

--- barhop/reset-database.bat
+++ barhop/reset-database.bat
@@ -1,14 +1,15 @@
-
 @echo off
 (FOR /d /r . %%d IN (migrations) DO @IF EXIST "%%d" rd /s /q "%%d") &&^
 echo Begin reset database. && echo Recreating database... &&^
 mysql -u afterhoursco -p -e "DROP DATABASE IF EXISTS barhop; CREATE DATABASE barhop;" ||^
 pause && exit &^
-echo Begin populating data. &&^
-mysql -u afterhoursco -p barhop < barhop_data.sql ||^
-pause && exit &^
 echo Database created successfully. Making migrations... &&^
 ..\venv\scripts\activate &&^
 python manage.py makemigrations user_management bars reservations reviews &&^
 python manage.py migrate && python manage.py createsuperuser --no-input && deactivate &&^
-echo Migrations successful. && pause
+echo Migrations successful. Begin populating data. &&^
+echo Please login to the MySQL AfterHoursCo account. &&^
+mysql -u afterhoursco -p barhop < barhop_data.sql ||^
+pause && exit &^
+echo Database setup complete.
+&& pause
--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Begin installation. &&^
+echo Begin installation. Please login to your MySQL root account. &&^
 mysql -u root -p -e "DROP DATABASE IF EXISTS BARHOP; CREATE DATABASE barhop; DROP USER IF EXISTS 'afterhoursco'@'%%'; CREATE USER 'afterhoursco'@'%%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON barhop.* TO 'afterhoursco'@'%';" ||^
 pause && exit &^
 echo MySQL Database setup successful. Installing packages... &&^
@@ -9,4 +9,8 @@ echo Python environment setup successful. Making migrations... &&^
 python manage.py makemigrations user_management bars reservations reviews && python manage.py migrate &&^
 echo Migrations successful. Creating Superuser... &&^
 python manage.py createsuperuser --no-input && deactivate &&^
+echo Migrations successful. Begin populating data. &&^
+echo Please login to the MySQL AfterHoursCo account. &&^
+mysql -u afterhoursco -p barhop < barhop_data.sql ||^
+pause && exit &^
 echo Installation successful. && pause
