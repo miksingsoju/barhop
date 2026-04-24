29ca2d1436f21cc50d985f54ecbdac86ba2c553b
ar-dimatulac <adara.dimatulac@student.ateneo.edu>
2026-04-24 17:37:39 +0800

    fix: custom time picker ui time format

--- barhop/bars/views.py
+++ barhop/bars/views.py
@@ -166,6 +166,9 @@ def bar_update(request, bar_id):
         return redirect('bars:bar-details', bar_id=bar_id)
     if request.method == 'POST':
         bar_form = CreateBarForm(request.POST, request.FILES, instance=bar_object)
+        if bar_images:
+            bar_image_formset = UpdateBarImageFormSet(queryset=bar_images)
+
         if bar_form.is_valid():
             bar = bar_form.save()
 
@@ -178,6 +181,8 @@ def bar_update(request, bar_id):
                 BarImage.objects.create(bar=bar, image=image_file)
 
             return redirect('bars:bar-details', bar_id=bar_id)
+        else:
+            print(bar_form.errors)
     else:
         bar_form = CreateBarForm(instance=bar_object)
         if bar_images:
